60f03bb72701b1076b66cda86a348ae175d00153
wyd-case <2777327669@qq.com>
2022-08-24 17:19:48 +0800

    final modify

--- src/main/java/com/ouyue/git/GitTest.java
+++ src/main/java/com/ouyue/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("modify test from gitee");
         System.out.println("modify test from idea to gitee");
         System.out.println("更新内容 到github仓库");
+        System.out.println("最新内容添加");
     }
 }
